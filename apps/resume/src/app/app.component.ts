5c5e882fa25d4e1f3ba6c91f16ecb9cefe18c011
Alexander Mahler <alex.domi.mahler@googlemail.com>
2023-03-29 21:51:37 +0200

    Chore(mono): linting rules (#1)
    * chore(mono): Add linting rules for typescript and format files according to them
    
    * chore(mono): HTML-Prettier rules added
    
    ---------
    
    Co-authored-by: Alexander Mahler <alexandermahler@MacBook-Pro-von-Alexander.local>

--- apps/resume/src/app/app.component.ts
+++ apps/resume/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'mono-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'mono-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'resume';
+    title = 'resume'
 }
--- apps/resume/src/app/app.module.ts
+++ apps/resume/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { NavBarComponent } from '@mono/resume/nav-bar';
-import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from './app.component'
+import { NxWelcomeComponent } from './nx-welcome.component'
+import { RouterModule } from '@angular/router'
+import { appRoutes } from './app.routes'
+import { NavBarComponent } from '@mono/resume/nav-bar'
+import { AppRoutingModule } from './app.routing.module'
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    AppRoutingModule,
-    NavBarComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NxWelcomeComponent],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+        AppRoutingModule,
+        NavBarComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
